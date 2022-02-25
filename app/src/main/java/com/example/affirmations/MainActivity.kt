ae2a96f3c4442d5339c00c26b0472ba8946deb2e
kubomnhata <55189177+kubomnhata@users.noreply.github.com>
2022-02-25 15:28:59 +0700

    final

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.DataSource
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         val myDataset = DataSource().loadAffirmations()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        ////// Horizontal
+        //val linear : LinearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        ////// Grid 2 columns
+        var linear : GridLayoutManager = GridLayoutManager(this, 2)
+        //////
+        recyclerView.layoutManager = linear
         recyclerView.adapter = ItemAdapter(this, myDataset)
         recyclerView.setHasFixedSize(true)
     }
